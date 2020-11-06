577930f2a4eba05238fe96e313b9d73451a722d0
omgMerrickd <merrickd@gmail.com>
2020-11-06 15:28:44 -0800

    Don't print newline

--- get-quote.py
+++ get-quote.py
@@ -7,7 +7,8 @@ def primary():
 
   last = 13
   rnd = random.randint(0, last)
-  print(quotes[rnd])
+  print(quotes[rnd], end = '')
+  print(quotes[rnd], end = '')
 
 if __name__== "__main__":
   primary()
